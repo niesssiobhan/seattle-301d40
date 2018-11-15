06f76ce7a75a25d153cc9a6a66522e7308103c31
Sam Hamm <sam@codefellows.com>
2018-11-14 17:16:12 -0800

    Publishes code challenge

--- 03-flexbox-templating/demo-handlebars/scripts/neighborhood.js
+++ 03-flexbox-templating/demo-handlebars/scripts/neighborhood.js
@@ -5,14 +5,20 @@ let neighborhoods = [];
 // REVIEW: This is another way to use a constructor to duplicate an array of raw data objects
 function Neighborhood (rawDataObject) {
   for (let key in rawDataObject) {
+    console.log('key', key);
     this[key] = rawDataObject[key];
   }
 }
 
 Neighborhood.prototype.toHtml = function() {
   // 1. Get the template from the HTML document
+  const source = $('#neighborhood-template').html();
+  // console.log(source);
   // 2. Use Handlebars to "compile" the HTML
+  const template = Handlebars.compile(source);
+  // console.log('template', template);
   // 3. Do not forget to return the HTML from this method
+  return template(this);
 };
 
 neighborhoodDataSet.forEach(neighborhoodObject => {
